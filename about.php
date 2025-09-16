9f0af9b19cbeaa9bffa2084ea45ea3466c019db0
Sandeepa Dilshan <sandeepadilshan783@gmail.com>
2025-09-16 18:43:04 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->    
+    <link rel="icon" type="image/jpeg" href="images/el.jpg">
+   <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<link href="home.css" rel="stylesheet" type="text/css">
+	<link href="about.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  	<div class="container-fluid">
+  	  <div class="container">
+  	    <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="Home.html"><img src="images/logo22.jpg" alt="" width="100" height="88" class="rounded img-fluid"/>&nbsp; Life Care Hospital&nbsp;</a>
+  	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  	      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+  	        <ul class="navbar-nav mr-auto">
+  	          <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="gallery.php">&nbsp;Gallery</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="About.php">&nbsp;About Us</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="Contacts.php">&nbsp;Contact Us</a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="Register.php">&nbsp;register</a> </li>
+
+            </ul>
+</div>
+        </nav>
+        <h1> Our Story&nbsp;</h1>
+        <div class="row">
+          <div class="col-lg-4"><img src="images/lifecare.jpg" alt="Placeholder image" height="532" class="img-fluid img-thumbnail"></div>
+          <div class="col-lg-4"><img src="images/2022-06-06.jpg" alt="Placeholder image" height="500" class="img-fluid img-thumbnail"></div>
+          <div class="col-lg-4"><img src="images/20f7c3de-3ea8-44ea-9256-a5084d9e68a1.jpg" class="img-fluid img-thumbnail" alt="Placeholder image"></div>
+        </div>
+        <p>Founded with a vision to provide compassionate and advanced healthcare, our hospital has grown into a trusted center of excellence. From humble beginnings, we have built a team of dedicated medical professionals, invested in modern technology, and created a healing environment where patients always come first. Our journey reflects a commitment to quality care, innovation, and the well-being of every individual who walks through our doors.</p>
+        <ul>
+          Through this Web Site, you can:
+        <li> Browse our medical services and facilities&nbsp;</li>
+        <li> &nbsp;Pre-book appointments and avoid long waiting times</li>
+        <li> &nbsp;Track your appointment status in real time</li>
+        <li> &nbsp;Receive updates on health tips, wellness programs, and special services</li>
+        </ul><p>We are committed to providing safe, affordable, and high-quality healthcare for all patients and families. Your well-being is our priority, and we’re constantly improving to serve you better.&nbsp;</p>
+        <ul>
+          Stay healthy with us!&nbsp;<br>
+		  Your Hospital Care Team
+        </ul>
+  	    <h1>Opening Hours&nbsp;</h1>
+  	    <h4>Monday to Friday&nbsp;</h4>
+<h3>24h&nbsp;&nbsp;</h3>
+<h4>Saturday to Sunday&nbsp;</h4>
+<h3>24h&nbsp;</h3>
+<p>____________________________________________________________________________________________________________________________________________________________&nbsp;</p>
+<div class="row">
+      <div class="col-lg-6">
+            <footer>About Us&nbsp;</footer>
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; With the freshness and original taste.&nbsp;</div>
+          <div class="col-lg-6">
+            <footer>
+              <p> Contacts</p>
+              <p>Tel : 011 5446000&nbsp;</p>
+            </footer>
+          &nbsp;</div>
+        </div>
+      </div>
+      </div>
+  	</div>
+  	<!-- body code goes here -->
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>

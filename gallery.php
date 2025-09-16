170d109e4977d77470eb04ced971b8cdc4eb613a
wdchathmal <wdchathmal@students.nsbm.ac.lk>
2025-09-16 15:38:02 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+        <link rel="icon" type="image/jpeg" href="images/el.jpg">
+  <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<link href="home.css" rel="stylesheet" type="text/css">
+	<link href="gallery.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  	<div class="container-fluid">
+  	  <div class="container">
+  	    <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="Home.html"><img src="images/logo22.jpg" alt="" width="100" height="88" class="rounded img-fluid"/>&nbsp; Life Care Hospital&nbsp;</a>
+  	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  	      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+  	        <ul class="navbar-nav mr-auto">
+  	          <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="gallery.php">&nbsp;Gallery</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="About.php">&nbsp;About Us</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="Contacts.php">&nbsp;Contact Us</a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="Register.php">&nbsp;register</a> </li>
+
+  	           
+            </ul>
+</div>
+        </nav>
+<h1>Gallery&nbsp;</h1>
+<h2>Newest Equipments&nbsp;</h2>
+<div class="row">
+  <div class="col-lg-4">
+<img src="images/b1.jpg" class="img-fluid" alt="Placeholder image">
+<p>&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+<img src="images/s320170718-12154-190v9f420170718-12154-mig3ia.jpg" alt="Placeholder image" width="600" class="img-fluid">
+<p>&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+<img src="images/Nurse-putting-patient-into-MRI-machine.jpg" alt="Placeholder image" width="960" height="500" class="img-fluid">
+<p>&nbsp;</p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4">
+    <img src="images/Medical-Device-Industry_1_770x548.png" class="img-fluid" alt="Placeholder image">
+    <p>&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/images (3).jpg" alt="Placeholder image" width="600" class="img-fluid">
+    <p>&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/EKG-800.jpg" alt="Placeholder image" width="1504" height="800" class="img-fluid">
+    <p>&nbsp;</p>
+  </div>
+</div>
+<p>____________________________________________________________________________________________________________________________________________&nbsp;</p>
+</p>
+<h2>Medical Staff&nbsp;</h2>
+<div class="row">
+  <div class="col-lg-4">
+    <img src="images/side-image.jpg" alt="Placeholder image" width="778" height="902" class="img-fluid">
+    <p>pediatrics&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/gallery_02.jpg" alt="Placeholder image" width="1000" height="1000" class="img-fluid">
+    <p>&nbsp;gynecologist</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/gallery_01.jpg" alt="Placeholder image" width="900" class="img-fluid">
+    <p>Dentist&nbsp;</p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4">
+    <img src="images/2a0e8cb609405d9ca87bc81154b9c443.jpg" class="img-fluid" alt="Placeholder image">
+    <p>Cardiologist&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/9d85ef63db3691882dee8b0d2dd08a4c.jpg" alt="Placeholder image" width="900" class="img-fluid">
+    <p>Dermatologist&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/61033501-healthcare-and-medical-concept-female-doctor-with-stethoscope.jpg" class="img-fluid" alt="Placeholder image">
+    <p>Neurologist&nbsp;</p>
+  </div>
+</div>
+<p>____________________________________________________________________________________________________________________________________________&nbsp;</p>
+<h2>Our Service&nbsp;</h2>
+<div class="row">
+  <div class="col-lg-4">
+    <img src="images/Types-of-Medical-Supplies-729119_1200x660.jpg" class="img-fluid" alt="Placeholder image">
+    <p>High Tech Medical Equipments&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/blog_04.jpg" class="img-fluid" alt="Placeholder image">
+    <p>Friendly Staff&nbsp;&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/GettyImages-530685401-59852f4c396e5a001100f8aa.jpg" alt="Placeholder image" width="1498" height="400" class="img-fluid">
+    <p>24/7 Emergrncy Service&nbsp;</p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-lg-4">
+    <img src="images/Banner-1.png" class="img-fluid" alt="Placeholder image">
+    <p>Online Appointments&nbsp;</p>
+  </div>
+  <div class="col-lg-4">
+    <img src="images/Customer-Support-Outsourcing-Department.png" alt="Placeholder image" height="600" class="img-fluid">
+    <p>24h Customer Service&nbsp;</p>
+  </div>
+  <div class="col-lg-4"> &nbsp;</div>
+</div>
+<p>____________________________________________________________________________________________________________________________________________&nbsp;</p>
+<div class="row">
+      <div class="col-lg-6">
+            <footer>About Us&nbsp;</footer>
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Finest Health Care in Sri Lanka&nbsp;</div>
+          <div class="col-lg-6">
+            <footer>
+              <p> Contacts</p>
+              <p>Tel : 011 5446000&nbsp;</p>
+            </footer>
+          &nbsp;</div>
+        </div>
+      </div>
+  	</div>
+  	<!-- body code goes here -->
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>

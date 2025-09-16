7e2b1a89fbe3b5ba76145d32f4d4d9e1f39b1e89
sinethrajapaksha082-prog <sinethrajapaksha082@gmail.com>
2025-09-16 15:36:49 +0530

    Add files via upload

---
+++ Contacts (1).php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+    <link rel="icon" type="image/jpeg" href="images/el.jpg">
+ <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	<link href="home.css" rel="stylesheet" type="text/css">
+	<style type="text/css">
+	@import url("contacts.css");
+    @import url("contacts.css");
+    @import url("contacts.css");
+    </style>
+</head>
+  <body>
+  	<div class="container-fluid">
+  	  <div class="container">
+  	    <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="Home.html"><img src="images/logo22.jpg" alt="" width="100" height="980" class="rounded img-fluid"/>&nbsp;&nbsp;Life Care Hospital</a>
+  	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  	      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+  	        <ul class="navbar-nav mr-auto">
+  	          <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="gallery.php">&nbsp;Gallery</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="About.php">&nbsp;About Us</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="Contacts.php">&nbsp;Contact Us</a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="Register.php">&nbsp;register</a> </li>
+
+  	          
+            </ul>
+</div>
+        </nav>
+
+<div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel" style="background-color: #087564">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
+  </ol>
+<div class="carousel-inner" role="listbox">
+  <div class="carousel-item active">
+    <h2><strong>Contact Life Care Hospital</strong></h2>
+    <h2>&nbsp;To contact us, patients must first register on our platform. After registration, they can easily book appointments and access all the services provided by our hospital. We’re here to support your healthcare needs every step of the way! If you have already registered simply SIGN IN and make appointments. THANK YOU.</h2>
+    <img class="d-block mx-auto" src="images/slider-1.jpg" alt="First slide">
+    <div class="carousel-caption"></div>
+  </div>
+
+  <div class="carousel-item">
+	   <h2><strong>Our Advanced Facilities<br>
+	  Equipped with modern technology to provide the best healthcare services</strong></h2>
+    <img class="d-block mx-auto" src="images/slider-3.jpg" alt="Second slide">
+    <div class="carousel-caption">
+     
+    </div>
+  </div>
+
+  <div class="carousel-item">
+	  <h2><strong>Dedicated Medical Staff<br>
+      Compassionate professionals ready to care for you 24/7.</strong></h2>
+    <img class="d-block mx-auto" src="images/slider-2.jpg" alt="Third slide">
+    <div class="carousel-caption">
+      
+    </div>
+  </div>
+</div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+<div class="row">
+      <div class="col-lg-6">
+            <footer>About Us&nbsp;</footer>
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Better Health with Life Care Hospital.&nbsp;</div>
+          <div class="col-lg-6">
+            <footer>
+              <p> Contacts</p>
+              <p>Tel : 011 5446000&nbsp;</p>
+            </footer>
+          &nbsp;</div>
+        </div>
+      </div>
+  	</div>
+  	<!-- body code goes here -->
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
---
+++ index (1).php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <!-- Bootstrap -->
+    <link rel="icon" type="image/jpeg" href="images/el.jpg">
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+    <style type="text/css">
+    @import url("home.css");
+    </style>
+</head>
+  <body>
+  	<div class="container-fluid">
+  	  <div class="container">
+  	    <nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="Home.html"><img src="images/logo22.jpg" alt="" width="100" height="980" class="rounded img-fluid"/>&nbsp; Life Care Hospital&nbsp;</a>
+  	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  	      <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+  	        <ul class="navbar-nav mr-auto">
+  	          <li class="nav-item active"> <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="gallery.php">&nbsp;Gallery</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="About.php">&nbsp;About Us</a> </li>
+			  <li class="nav-item"> <a class="nav-link" href="Contacts.php">&nbsp;Contact Us</a> </li>
+  	          <li class="nav-item"> <a class="nav-link" href="register.php">&nbsp;Register</a> </li>
+
+  	          
+            </ul>
+</div>
+        </nav>
+        <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel" style="background-color: #087564">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active"> <img src="images/slider-1.jpg" alt="First slide" width="1573" height="626" class="d-block mx-auto img-fluid rounded">
+              <div class="carousel-caption">
+                <h5>Life Care Hospital&nbsp;</h5>
+                <p>Sri Lankan finest health care</p>
+              </div>
+            </div>
+            <div class="carousel-item"> <img class="d-block mx-auto" src="images/slider-2.jpg" alt="Second slide">
+              <div class="carousel-caption">
+                <h5>Life Care Hospital</h5>
+                <p>Newest Equipments and Service</p>
+              </div>
+            </div>
+            <div class="carousel-item"> <img class="d-block mx-auto" src="images/slider-3.jpg" alt="Third slide">
+              <div class="carousel-caption">
+                <h5>Life Care Hospital</h5>
+                <p>Best Medical Staff</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span></a></div>
+        <h1>&nbsp;WELCOME to LIFE HOSPITAL&nbsp;</h1>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card col-md-4 col-lg-12"> <img class="card-img-top" src="images/blog_02.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Best Equipments&nbsp;</h5>
+                <h6>We are equipped with the latest medical technology to provide safe, accurate, and reliable healthcare services. From advanced patient monitoring systems and diagnostic machines (X-ray, MRI, Ultrasound) to modern surgical instruments and laboratory analyzers, our hospital ensures high-quality care and precise treatment for every patient.&nbsp;
+                </h6>
+<a href="Register.php" class="btn btn-primary">Register&nbsp;</a> </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card col-md-4 col-lg-12"> <img src="images/hospital-banner.jpg" alt="Card image cap" height="400" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Patient Care&nbsp;</h5>
+                <h6>We prioritize every patient’s health and comfort, offering compassionate, personalized, and professional care. Our experienced medical team ensures timely treatment, continuous monitoring, and emotional support throughout the recovery process, making sure every patient feels safe, respected, and valued.&nbsp;</h6>
+<a href="Register.php" class="btn btn-primary">Make Appointment</a> </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card col-md-4 col-lg-12"> <img class="card-img-top" src="images/blog_03.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Finest Medical Staff&nbsp;</h5>
+                <h6>Our hospital is proud to have a team of highly skilled and dedicated doctors, nurses, and healthcare professionals. With extensive experience, continuous training, and a patient-first approach, our staff ensures top-quality care, accurate diagnoses, and effective treatments for every patient.&nbsp;</h6>
+<a href="Register.php" class="btn btn-primary">Log In&nbsp;</a> </div>
+            </div>
+          </div>
+        </div>
+        <div id="accordion1" role="tablist">
+          <div class="card">
+            <div class="card-header" role="tab" id="headingOne1">
+              <h5 class="mb-0"> <a data-toggle="collapse" href="#collapseOne1" role="button" aria-expanded="true" aria-controls="collapseOne1"> Gallery&nbsp; </a> </h5>
+            </div>
+            <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordion1">
+              <div class="card-body">&nbsp; &nbsp; <a href="gallery.html">Our newest equipments&nbsp; and medical stafff</a></div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" role="tab" id="headingTwo1">
+              <h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseTwo1" role="button" aria-expanded="false" aria-controls="collapseTwo1"> About Us&nbsp; </a> </h5>
+            </div>
+            <div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1" data-parent="#accordion1">
+               <div class="card-body">&nbsp; &nbsp; <a href="about.html">Our Story</a></div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" role="tab" id="headingThree1">
+              <h5 class="mb-0"> <a class="collapsed" data-toggle="collapse" href="#collapseThree1" role="button" aria-expanded="false" aria-controls="collapseThree1"> Contacts&nbsp; </a> </h5>
+            </div>
+            <div id="collapseThree1" class="collapse" role="tabpanel" aria-labelledby="headingThree1" data-parent="#accordion1">
+               <div class="card-body">&nbsp; &nbsp; <a href="Register.html">Register for make Appointments</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <footer>About Us&nbsp;</footer>
+            <p> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; With the Finest Health care.&nbsp;</p>
+            <p>Address</p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Life Hospital,pitipana Road,Thalagala,Homagama</p>
+            <p>&nbsp;</p>
+          </div>
+          <div class="col-lg-6">
+            <footer>
+              <p> Contacts</p>
+              <p>Tel : 011 5446000&nbsp;</p>
+            </footer>
+          &nbsp;</div>
+        </div>
+      </div>
+  	</div>
+  	<!-- body code goes here -->
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>

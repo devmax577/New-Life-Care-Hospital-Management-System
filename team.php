69f20df2524d8a39f606157d374a82d0b450bfd2
adee2002 <adeekarunarathne@gmail.com>
2025-09-16 16:09:56 +0530

    Add files via upload

---
+++ team.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Life Hospitals(Pvt)Ltd</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+    <body>
+    
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-md-12 left-item">
+                        <ul>
+                            <li>Welcome to Life Hospital</li>
+                            
+                        </ul>
+                    </div>
+                    <div class="col-lg-5 d-none d-lg-block right-item">
+                        <ul>
+                             <li><i class="fas fa-envelope-square"></i> Lifehospital@sl.com</li>
+                             <li><i class="fas fa-phone-square"></i> +94 763998720</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-12 nav-img">
+                        <img src="assets/images/logo2.bmp" alt="">
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                           <li><a href="team.php">Home</a></li>
+                            <li><a href="appoint.php">Make Appointments</a></li>
+                            
+                            <li><a href="psearch.php">Medical Records</a></li>
+                            
+                            <li><a href="index.php">Main Menu</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </header>
+               <div class="page-nav no-margin row">
+                   <div class="container">
+                       <div class="row">
+                          
+                       </div>
+                   </div>
+               </div>
+       
+   
+       
+    <section class="our-team team-11">
+        <div class="container">
+            
+            <div class="row team-row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/team-memb1.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr R.R.Jamaldeen</h4>
+                            <i>Neurologist</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/m1.png" alt="">
+                        <div class="det-o">
+                            <h4>Dr Y.S.Thennakoon</h4>
+                            <i>Dermatologist</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/m4.jfif" alt="">
+                        <div class="det-o">
+                            <h4>Dr K.M.Meegama</h4>
+                            <i>Obstetrician-gynecologist</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/team-memb4.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr S.M.K.Perera</h4>
+                            <i>Cardiologist</i>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/m2.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr H.S.Rajapaksha</h4>
+                            <i>Gastroenterologist</i>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/m3.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr J.S.Jayathissa</h4>
+                            <i>Oncologist</i>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/team-memb2.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr W.P.S.Udeni</h4>
+                            <i>Psychiatrist</i>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6">
+                    <div class="single-usr">
+                        <img src="assets/images/team/team-memb3.jpg" alt="">
+                        <div class="det-o">
+                            <h4>Dr N.M.Manjula</h4>
+                            <i>Pediatrician</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+        
+
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <h2>About Us</h2>
+                    <p>
+                        Welcome to Life Care Hospital, where your health and well-being are our top priorities. Established with a commitment to providing exceptional medical care, Life Hospital is a leading healthcare institution dedicated to offering comprehensive and compassionate healthcare services to our community
+                    </p>
+                    <p>At Life Hospital, our mission is to deliver high-quality, patient-centered care with a focus on safety, dignity, and respect. We strive to improve the health of our patients by offering advanced medical treatments and compassionate support</p>
+                </div>
+               
+                <div class="col-md-4 col-sm-12 map-img">
+                    <h2>Contact Us</h2>
+                    <address class="md-margin-bottom-40">
+                         Homagama <br>
+                        Biyagama(Colombo District) <br>
+                        Western Province,SL <br>
+                        Phone: +94 763998720 <br>
+                        Email: <a href="mailto:info@anybiz.com" class="">Lifehospital@sl.com</a><br>
+                        
+                    </address>
+
+                </div>
+            </div>
+        </div>
+        
+
+    </footer>
+    
